bbe3ba848b61cc8c0992d58cd263bdb318884f1c
wayne <wayne.liu@nih.gov>
2016-03-17 16:26:03 -0400

    wk06 hw flask, committed hw06

--- Wk06/06-storing-input/site.py
+++ Wk06/06-storing-input/site.py
@@ -11,8 +11,9 @@ from wtforms.validators import DataRequired
 
 
 
-class MyForm(Form, text='first name'):
-    name = StringField(text, validators=[DataRequired()])
+class MyForm(Form):
+	name = StringField('first name', validators=[DataRequired()])
+	name1 = StringField('last name', validators=[DataRequired()])
 	
 
 from flask_sqlalchemy import SQLAlchemy
@@ -24,29 +25,33 @@ db = SQLAlchemy(app)
 class Visitor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
+    name1 = db.Column(db.String(80))
 
 
-    def __init__(self, name):
+    def __init__(self, name, name1):
         self.name = name
-    
+        self.name1 = name1
+
 
     def __repr__(self):
-        return '<Visitor %r>' % self.name
+        return '<Visitor %r, %r>' % self.name, self.name1
 	
 	
 db.create_all()
 	
 @app.route('/', methods=('GET', 'POST'))
 def index():
-	form = MyForm('first name')
+	form = MyForm(text='first name')
 	previous_visitors = Visitor.query.order_by('id DESC').all()
 	if form.validate_on_submit():
 		name = form.name.data
-		db.session.add(Visitor(name))
+		name1 = form.name1.data
+		db.session.add(Visitor(name, name1))
 		db.session.commit()
 	else:
-		name = 'stranger'
-	return render_template('index.html', form=form, name=name, previous_visitors=previous_visitors)
+		name = 'Doctor'
+		name1 = 'Strange'
+	return render_template('index.html', form=form, name=name, name1=name1, previous_visitors=previous_visitors)
 	
 @app.route('/custom/<custom>/')
 def custom(custom):
